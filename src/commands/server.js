e791f2c80cae70d6f8d7d49c333abb5d452ef7f0
myin <minyin142p@gmail.com>
2024-01-24 19:35:44 +0100

    add start/stop commands

--- src/commands/server.js
+++ src/commands/server.js
@@ -1,41 +1,87 @@
-const { spawn } = require("child_process");
+const { spawn, execFile } = require("child_process");
 
 const runCmd = (cmd) => {
   return new Promise((resolve, reject) => {
-    const child = spawn("sh", ["-c", cmd]);
+    execFile("sh", ["-c", cmd], (error, stdout, stderr) => {
+      if (error) {
+        console.log("Received error: " + error);
+        reject(error);
+      }
 
-    child.stdout.on("data", (data) => {
-      console.log('Received data: ' + data);
-      resolve(data + '');
+      console.log("Received data: " + stdout);
+      resolve(stdout + "");
     });
+  });
+};
 
+const startProgram = async (cmd) => {
+  return new Promise((resolve, reject) => {
+    const child = spawn("sh", ["-c", cmd]);
+    child.stdout.on("data", (data) => {
+      resolve(data + "");
+    });
     child.stderr.on("data", (data) => {
-      console.log('Received error: ' + data);
       reject(data);
     });
-
     child.addListener("exit", (code) => {
-      console.log('Received exit code: ' + code);
+      console.log("Received exit code: " + code);
       resolve("");
     });
   });
 };
 
+const startPalServer = async (runner) => {
+  try {
+    console.log("Starting PalServer");
+
+    const id = await getPalServerPID();
+    if (id != null) {
+      runner.send("PalServer is still running. Stop it first.");
+      return;
+    }
+
+    const line = await startProgram("$HOME/palserver.sh");
+    console.log("Started pal server " + line);
+  } catch (e) {
+    runner.send("Failed to start server: " + e);
+  }
+};
+
+const stopPalServer = async (runner) => {
+  try {
+    console.log("Stopping PalServer");
+
+    const id = await getPalServerPID();
+    if (id == null) {
+      runner.send("PalServer is not running");
+      return;
+    }
+
+    const line = await killProgram(id);
+    runner.send("PalServer is stopping");
+    console.log("Stopping: " + line);
+  } catch (e) {
+    runner.send("Failed to stop server: " + e);
+  }
+};
+
 const zerotierStatus = async () => {
-  console.log('Check zerotier status');
+  console.log("Check zerotier status");
   const countStr = await runCmd('netstat -lnut | grep ":9993" | wc -l');
   const count = parseInt(countStr);
   return count > 0;
 };
 
 const getPalServerPID = async () => {
-  console.log('Check pal server status');
-  const line = await runCmd(`ps -a | grep "palserver.sh" | awk '{ print $1 }'`);
+  console.log("Check pal server status");
+  const line = await runCmd(
+    `ps -a | grep "PalServer-Linux" | awk '{ print $1 }'`
+  );
 
   if (!line || line.trim() !== "") {
     const x = parseInt(line);
     if (isNaN(x)) {
-        return null
+      return null;
     }
     return x;
   }
@@ -43,16 +89,21 @@ const getPalServerPID = async () => {
   return null;
 };
 
+const killProgram = async (pid) => {
+  console.log(`Killing program with pid ${pid}`);
+  await runCmd(`kill ${pid}`);
+};
+
 const serverStatus = async (runner) => {
   try {
     const isOnline = await zerotierStatus();
     const palID = await getPalServerPID();
     runner.send(`Zerotier: ${isOnline ? "online" : "offline"}
-PalServer: ${palID != null ? 'online' : 'offline'}
+PalServer: ${palID != null ? "online" : "offline"}
 `);
   } catch (e) {
     runner.send(`Error: ${e}`);
   }
 };
 
-module.exports = { serverStatus };
+module.exports = { serverStatus, startPalServer, stopPalServer };
